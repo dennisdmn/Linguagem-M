3b2d5f748ddebda7bb3d0ed36ddbc4a594f10984
dennisdmn <34797928+dennisdmn@users.noreply.github.com>
2025-03-20 09:49:46 -0300

    Create ExtracaoNumerosDeTextosNovaColuna.m
    Implementação de uma função personalizada em Power Query (M) para extrair números de uma string e converter para número inteiro, além de adicionar uma nova coluna com o nome especificado.
    
    Funções principais:
    - `ExtractNumbersAsInt`: Extrai apenas os caracteres numéricos de uma string e os converte em um número inteiro.
    - `AdicionarColunaComNome`: Adiciona uma nova coluna a uma tabela existente, utilizando a função `ExtractNumbersAsInt` para preencher a nova coluna com números extraídos de uma coluna existente.
    
    ## Detalhes da implementação:
    
    - `ExtractNumbersAsInt`:
      - Parâmetro: `inputText` (texto)
      - Extrai caracteres numéricos (0 a 9) da string fornecida.
      - Converte o texto extraído em um número inteiro.
    
    - `AdicionarColunaComNome`:
      - Parâmetros: `tabela` (tabela), `colunaNome` (texto)
      - Adiciona uma nova coluna à tabela fornecida.
      - Utiliza a função `ExtractNumbersAsInt` para preencher a nova coluna com os números extraídos da coluna `[DESCRICAO_PERIODO]`.
    
    - Função final:
      - Aceita uma tabela e um nome de coluna como parâmetros.
      - Retorna a tabela com a nova coluna adicionada.
    
    ## Exemplo de uso:
    
    ```m
    let
        // Substitua 'ColunaTipoPeriodo' pelo nome da sua tabela
        Source = ColunaTipoPeriodo,
        // Nome da nova coluna
        NomeNovaColuna = "QTDE_PERIODOS",
        // Adiciona a nova coluna utilizando a função personalizada
        Resultado = AdicionarColunaComNome(Source, NomeNovaColuna)
    in
        Resultado

---
+++ TEXTO/ExtracaoNumerosDeTextosNovaColuna.m
@@ -0,0 +1,21 @@
+let
+    // Função personalizada para extrair números de uma string e converter para número inteiro
+    ExtractNumbersAsInt = (inputText as text) as number =>
+    let
+        // Extrair apenas os caracteres que são números
+        extractedText = Text.Select(inputText, {"0".."9"}),
+        // Converter o texto extraído para número
+        resultNumber = Number.FromText(extractedText)
+    in
+        resultNumber,
+
+    // Função personalizada para adicionar uma nova coluna com o nome especificado
+    AdicionarColunaComNome = (tabela as table, colunaNome as text) as table =>
+    let
+        // Adiciona uma nova coluna com o nome especificado utilizando a função personalizada
+        NovaTabela = Table.AddColumn(tabela, colunaNome, each ExtractNumbersAsInt([DESCRICAO_PERIODO]), Int64.Type)
+    in
+        NovaTabela
+in
+    // Função que aceita uma tabela e uma coluna para o nome da nova coluna
+    (tabela as table, colunaNome as text) as table => AdicionarColunaComNome(tabela, colunaNome)
